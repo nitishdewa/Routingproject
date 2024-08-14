76944c994057faaa20b50d8d1622f806e5e8d98d
Nitish Dewangan <nitishdewangan2@gmail.com>
2024-08-14 16:52:26 +0530

    ref

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,12 +1,15 @@
 
-export const is = []; 
+export const is = [];
 
 export const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD_DATA':
             return [...state, action.payload];
         case 'REMOVE_DATA':
-            return state.filter((_, index) => index !== action.payload);
+            const newState = [...state]; // Create a shallow copy of the state array
+            newState.splice(action.payload, 1); // Remove the item at the index specified by action.payload
+            return newState;
+
         default:
             return state;
     }
