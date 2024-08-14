d2fa3257a63a30685575a8fc785454db2de68a87
Nitish Dewangan <nitishdewangan2@gmail.com>
2024-08-14 16:55:57 +0530

    image

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,12 @@
 const Home = () => {
     return (
-        <> 
-        
+        <>
+
             <h2>Student Record Management System</h2>
+            <img src="Images\Student-Record-Management-System.jpg" alt="student records" />
             <p>
-                Welcome to the Student Record Management System! This application is designed to help educational institutions 
-                efficiently manage student records, from admissions to graduation. Our system provides a user-friendly interface 
+                Welcome to the Student Record Management System! This application is designed to help educational institutions
+                efficiently manage student records, from admissions to graduation. Our system provides a user-friendly interface
                 that allows administrators to store, update, and retrieve student information with ease.
             </p>
             <p>
@@ -13,8 +14,8 @@ const Home = () => {
                 By utilizing this system, schools can ensure that all student data is securely stored and easily accessible whenever needed.
             </p>
             <p>
-                Whether you're managing records for a small school or a large university, our Student Record Management System 
-                is designed to scale with your needs. Stay organized, save time, and focus on providing the best educational 
+                Whether you're managing records for a small school or a large university, our Student Record Management System
+                is designed to scale with your needs. Stay organized, save time, and focus on providing the best educational
                 experience for your students.
             </p>
         </>
